97951a8bdbf1aee0093ad57a2939b6f58c5e0501
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-03 13:13:55 +0530

    WPF Login Form Creation
    Designed the Login Form and basic Validation Check

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -6,7 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
+using TRMDesktopUI.Helpers;
 using TRMDesktopUI.ViewModels;
 
 namespace TRMDesktopUI
@@ -18,6 +20,11 @@ namespace TRMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
