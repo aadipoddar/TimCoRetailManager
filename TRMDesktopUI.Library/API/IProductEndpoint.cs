84803a5256b398d11b754a0b36d9091132bbc564
Aadi Poddar <aadipoddarmail@gmail.com>
2022-02-08 16:40:27 +0530

    Display Product Data
    Display Products From the Database to the Sales Page

---
+++ TRMDesktopUI.Library/API/IProductEndpoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using TRMDesktopUI.Library.Models;
+
+namespace TRMDesktopUI.Library.API
+{
+    public interface IProductEndpoint
+    {
+        Task<List<ProductModel>> GetAll();
+    }
+}
