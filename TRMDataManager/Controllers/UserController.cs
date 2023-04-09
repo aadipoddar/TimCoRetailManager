c875c4e425d61e9935a14d125b0f6a63b709dcd1
Aadi Poddar <aadipoddarmail@gmail.com>
2023-04-09 15:05:45 +0530

    Getting User Data
    Getting User Information in the web api from the Database

---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Web.Http;
+
+using Microsoft.AspNet.Identity;
+
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public UserModel GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId).First();
+        }
+    }
+}
